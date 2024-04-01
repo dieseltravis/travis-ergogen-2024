af8ee7f6efa130f8c864e0d10708e96b0b388b7f
Travis Hardiman <dieseltravis@users.noreply.github.com>
2024-03-31 22:11:46 -0400

    wired v5

--- firmware/qmk/config.h
+++ firmware/qmk/config.h
@@ -25,11 +25,11 @@
 
 //TODO:
 //#define MATRIX_ROW_PINS { B6, B2, B3, B1, F7 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3 }
-#define MATRIX_COL_PINS { F4, F5 }
+#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
+#define MATRIX_COL_PINS { }
 //TODO:
 //#define MATRIX_COL_MUX_PINS { D7, E6, B4, B5 }
-#define MATRIX_COL_MUX_PINS { D1, D0, D4, C6 }
+#define MATRIX_COL_MUX_PINS { C6, D4, D0, D1 }
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 10
